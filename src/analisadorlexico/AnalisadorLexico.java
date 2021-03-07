d6b8c3819143532fae372b71d15f7e005537084a
Upper Rocks <lucalmendes9@gmail.com>
2021-03-07 20:59:18 -0300

    add - generate file and find reserved

--- src/analisadorlexico/AnalisadorLexico.java
+++ src/analisadorlexico/AnalisadorLexico.java
@@ -1,21 +1,46 @@
 package analisadorlexico;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+import java.util.Arrays;
 
 
 public class AnalisadorLexico {
 
     public static void main(String[] args) throws IOException {
-        String filePath = "/home/upper/Área de Trabalho/AnalisadorLexico/src/analisadorlexico/jquery-3.6.0.js";
+        String[] reserved = {"break", "case", "catch", "continue", "debugger", "default", "delete", "do", "else", "finally", "for", "function", "if", "in", "instanceof", "new", "return", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "class", "const", "enum", "export", "extends", "import", "super", "implements", "interface", "let", "package", "private", "protected", "public", "static", "yield"};
+        
+        String filePath = "src/analisadorlexico/jquery-3.6.0.js";
         String fileString = readLineByLineJava8( filePath );
         String withoutComments = fileString.replaceAll("(?s:/\\*.*?\\*/)|//.*", "");
-        String withoutSpaces = withoutComments.replaceAll("(?s)\\s|/\\*.*?\\*/|//[^\\r\\n]*", "" );
+        String withoutBreakLines = withoutComments.replaceAll("(?s)\\s|/\\*.*?\\*/|//[^\\r\\n]*", " " );
+        String withoutTabs = withoutBreakLines.trim().replaceAll(" +", " ");
+        
+        String[] words = withoutTabs.split(" ");
+        String[] alreadyFind = new String[words.length];
 
-        System.out.println( withoutSpaces );
+        Integer cont = 0;
+        
+        try (PrintWriter out = new PrintWriter("src/analisadorlexico/saida.lex.csv")) {
+            out.println("Numero Coluna,Token, Tipo");
+            
+            for(int i = 0; i < words.length; i++) {
+                
+                if(Arrays.asList(alreadyFind).contains(words[i])){
+                    continue;
+                }
+                if( Arrays.asList(reserved).contains(words[i]) ){
+                    alreadyFind[cont] = words[i];
+                    out.println(cont+","+words[i]+",Reservada");
+                    cont++;
+                    continue;
+                }
+                
+            }
+        }
     }
     
      
